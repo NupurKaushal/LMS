7ebae5d1ee5959c3b93d462d15572cc6a0461f85
intakhab08 <intakhab1503@gmail.com>
2025-02-27 14:53:17 +0530

    test another yml

--- src/main/java/com/library/Library/model/Admin.java
+++ src/main/java/com/library/Library/model/Admin.java
@@ -4,16 +4,22 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Entity
+@Entity
 @Data
 @NoArgsConstructor
 public class Admin {
 
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "LONG")
+    private long id;
 
+    @Column(nullable = false, columnDefinition = "VARCHAR(255)")
     private String name;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 }
